fc179400dc3ad643753847439ddc5fe81871b009
beboche <david.baux@inserm.fr>
2026-02-06 16:35:23 +0100

    added new script to fix a bug in wt_seq for some variants

---
+++ update_wrong_mt_seqs.py
@@ -0,0 +1,88 @@
+import re
+import psycopg2
+import psycopg2.extras
+from precompute_spipv2 import get_db, log
+# requires MobiDetails config module + database.ini file
+from MobiDetailsApp import md_utilities
+
+
+def main():
+    # script meant to update mt seq that from 20251201, 
+    # for variants lying in genes in minus strand, mutant sequence was wrong
+    # need to be ran on prod
+    # concerns all subsitutions when wt_seq = mt_seq and
+    # all delins, ins and dups lying in minus strand genes
+    # created after 20251201 by vcf_str method (no means to check this)
+    db_pool, db = get_db()
+    curs = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    # get wrong substitutions
+    curs.execute(
+        """
+        SELECT *
+        FROM variant_feature
+        WHERE wt_seq = mt_seq
+        """
+    )
+    subs_vars = curs.fetchall()
+    for var in subs_vars:
+        log("info", f"variant: {var['c_name']} - {var['refseq']}")
+        # slice mt_seq
+        mt_seq_obj = re.search(r'^([ATGC]+)\s[ATGC]\s([ATGC]+)$', var['mt_seq'])
+        if mt_seq_obj:
+            log("debug", f"variant: {var['c_name']} - {var['refseq']}")
+            begin = mt_seq_obj.group(1)
+            end = mt_seq_obj.group(2)
+            mt_sub_obj = re.search(r'>([ATGC])$', var['c_name'])
+            if mt_sub_obj:
+                new_mt_seq = f"{begin} {mt_sub_obj.group(1)} {end}"
+                log("info", f"UPDATE variant_feature SET mt_seq = {new_mt_seq} WHERE id = {var['id']}")
+                # curs.execute(
+                #     """
+                #     UPDATE variant_feature
+                #     FSET mt_seq = %s
+                #     WHERE id = %s
+                #     """,
+                #     (new_mt_seq, var['id'])
+                # )
+
+    # correct ins
+    curs.execute(
+        """
+        SELECT *
+        FROM variant_feature a, gene b
+        WHERE a.refseq = b.refseq
+            AND a.dna_type IN ('insertion', 'indel')
+            AND a.creation_date >= '2025-12-01'
+            AND b.strand = '-'
+        """
+    )
+    other_vars = curs.fetchall()
+    for var in other_vars:
+        log("info", f"variant: {var['c_name']} - {var['refseq']}")
+        # slice mt_seq
+        mt_seq_obj = re.search(r'^([ATGC]+)\s([ATGC])-*\s([ATGC]+)$', var['mt_seq'])
+        if mt_seq_obj:
+            log("debug", f"variant: {var['c_name']} - {var['refseq']}")
+            begin = mt_seq_obj.group(1)
+            middle = mt_seq.group(2)
+            end = mt_seq_obj.group(3)
+            mt_sub_obj = re.search(r'ins([ATGC]+)$', var['c_name'])
+            if mt_sub_obj.group(1) != middle:
+                # wrong
+                new_mt_seq = f"{begin} {mt_sub_obj.group(1)} {end}"
+                log("info", f"UPDATE variant_feature SET mt_seq = {new_mt_seq} WHERE id = {var['id']}")
+                # curs.execute(
+                #     """
+                #     UPDATE variant_feature
+                #     FSET mt_seq = %s
+                #     WHERE id = %s
+                #     """,
+                #     (new_mt_seq, var['id'])
+                # )
+    # db.commit()
+    db_pool.putconn(db)
+
+
+if __name__ == '__main__':
+    main()
