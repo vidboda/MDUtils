1848006d472d65ba81396eaecc8634ab38c248f9
beboche <david.baux@inserm.fr>
2026-02-06 17:36:08 +0100

    added cases where the wt was wrong

--- update_wrong_mt_seqs.py
+++ update_wrong_mt_seqs.py
@@ -22,30 +22,53 @@ def main():
         SELECT *
         FROM variant_feature
         WHERE wt_seq = mt_seq
+        ORDER by creation_date
         """
     )
     subs_vars = curs.fetchall()
     for var in subs_vars:
         log("info", f"variant: {var['c_name']} - {var['refseq']}")
         # slice mt_seq
-        mt_seq_obj = re.search(r'^([ATGC]+)\s[ATGC]\s([ATGC]+)$', var['mt_seq'])
+        mt_seq_obj = re.search(r'^([ATGC]+)\s([ATGC])\s([ATGC]+)$', var['mt_seq'])
         if mt_seq_obj:
-            log("debug", f"variant: {var['c_name']} - {var['refseq']}")
+            # log("debug", f"variant: {var['c_name']} - {var['refseq']}")
             begin = mt_seq_obj.group(1)
-            end = mt_seq_obj.group(2)
+            middle = mt_seq_obj.group(2)
+            end = mt_seq_obj.group(3)
             mt_sub_obj = re.search(r'>([ATGC])$', var['c_name'])
             if mt_sub_obj:
-                new_mt_seq = f"{begin} {mt_sub_obj.group(1)} {end}"
-                log("info", f"UPDATE variant_feature SET mt_seq = {new_mt_seq} WHERE id = {var['id']}")
-                # curs.execute(
-                #     """
-                #     UPDATE variant_feature
-                #     FSET mt_seq = %s
-                #     WHERE id = %s
-                #     """,
-                #     (new_mt_seq, var['id'])
-                # )
-
+                if middle != mt_sub_obj.group(1):
+                    # change mt_seq
+                    new_mt_seq = f"{begin} {mt_sub_obj.group(1)} {end}"
+                    log("info", f"UPDATE variant_feature SET mt_seq = {new_mt_seq} WHERE id = {var['id']}")
+                    curs.execute(
+                        """
+                        UPDATE variant_feature
+                        SET mt_seq = %s
+                        WHERE id = %s
+                        """,
+                        (new_mt_seq, var['id'])
+                    )
+                else:
+                    # change wt_seq ?
+                    wt_seq_obj = re.search(r'^([ATGC]+)\s([ATGC])\s([ATGC]+)$', var['wt_seq'])
+                    if wt_seq_obj:
+                        begin = wt_seq_obj.group(1)
+                        middle = wt_seq_obj.group(2)
+                        end = wt_seq_obj.group(3)
+                        wt_sub_obj = re.search(r'([ATGC])>', var['c_name'])
+                        if wt_sub_obj:
+                            new_wt_seq = f"{begin} {wt_sub_obj.group(1)} {end}"
+                            log("info", f"UPDATE variant_feature SET wt_seq = {new_wt_seq} WHERE id = {var['id']}")
+                            curs.execute(
+                                """
+                                UPDATE variant_feature
+                                SET wt_seq = %s
+                                WHERE id = %s
+                                """,
+                                (new_wt_seq, var['id'])
+                            )
+    db.commit()
     # correct ins
     curs.execute(
         """
@@ -63,7 +86,7 @@ def main():
         # slice mt_seq
         mt_seq_obj = re.search(r'^([ATGC]+)\s([ATGC]+)-*\s([ATGC]+)$', var['mt_seq'])
         if mt_seq_obj:
-            log("debug", f"variant: {var['c_name']} - {var['refseq']}")
+            # log("debug", f"variant: {var['c_name']} - {var['refseq']}")
             begin = mt_seq_obj.group(1)
             middle = mt_seq_obj.group(2)
             end = mt_seq_obj.group(3)
@@ -72,15 +95,15 @@ def main():
                 # wrong
                 new_mt_seq = f"{begin} {mt_sub_obj.group(1)} {end}"
                 log("info", f"UPDATE variant_feature SET mt_seq = {new_mt_seq} WHERE id = {var['id']}")
-                # curs.execute(
-                #     """
-                #     UPDATE variant_feature
-                #     FSET mt_seq = %s
-                #     WHERE id = %s
-                #     """,
-                #     (new_mt_seq, var['id'])
-                # )
-    # db.commit()
+                curs.execute(
+                    """
+                    UPDATE variant_feature
+                    SET mt_seq = %s
+                    WHERE id = %s
+                    """,
+                    (new_mt_seq, var['id'])
+                )
+    db.commit()
     db_pool.putconn(db)
 
 
