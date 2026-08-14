2f2332ece28f54beb630816fc0621762e541db5f
beboche <david.baux@inserm.fr>
2026-08-14 11:52:00 +0200

    added some info

--- md2bed.sh
+++ md2bed.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 DATE=$(date +%Y%m%d)
-FILENAME="/home/mobidetails/mddev_export_vars_${DATE}.bed"
+FILENAME="mddev_export_vars_${DATE}.bed"
 
-echo "Export vers $FILENAME..."
+echo "Export vers ${FILENAME}..."
+
+# echo "track type=bigBed name='MobiDetails variants' description='A bigBed File with all MobiDetails variants, date ${DATE}' bigDataUrl=https://mobidetails.chu-montpellier.fr/mdbigbed.bb url=https://mobidetails.chu-montpellier.fr/api/variant/\${id}/browser" > "${FILENAME}"
 
 psql -p 5433 -d mobidetails -c "\copy (
     SELECT
-        CONCAT('chr', chr) AS chrom,
+        CONCAT('chr', a.chr) AS chrom,
         CASE
-            WHEN g_name LIKE '%del%' THEN pos
-            ELSE pos - 1
+            WHEN a.g_name LIKE '%del%' THEN a.pos
+            ELSE a.pos - 1
         END AS chromStart,
         CASE
-            WHEN g_name LIKE '%ins%' THEN pos
-            WHEN g_name LIKE '%dup%' THEN pos + LENGTH(COALESCE(pos_alt, '')) - 1
-            WHEN g_name LIKE '%del%' THEN pos + LENGTH(COALESCE(pos_ref, '')) - 1
-            ELSE pos + GREATEST(LENGTH(COALESCE(pos_ref, '')), LENGTH(COALESCE(pos_alt, ''))) - 1
+            WHEN a.g_name LIKE '%dup%' THEN a.pos + LENGTH(COALESCE(a.pos_alt, '')) - 1
+            WHEN a.g_name LIKE '%del%' THEN a.pos + LENGTH(COALESCE(a.pos_ref, '')) - 1
+            WHEN a.g_name LIKE '%ins%' THEN a.pos
+            ELSE a.pos + GREATEST(LENGTH(COALESCE(a.pos_ref, '')), LENGTH(COALESCE(a.pos_alt, ''))) - 1
         END AS chromEnd,
-        CONCAT('chr', chr, ':g.', g_name) AS name,
-        0, '+',
-        CONCAT('https://mobidetails.chu-montpellier.fr/api/variant/', feature_id, '/browser')
-    FROM variant
-    WHERE pos IS NOT NULL
-        AND genome_version = 'hg38'
+        CONCAT('chr', a.chr, ':g.', a.g_name) AS name,
+        0,
+        b.strand,
+        a.feature_id,
+        c.gene_symbol,
+        CONCAT(c.refseq, ':c.', c.c_name)
+    FROM variant a, gene b, variant_feature c
+    WHERE a.feature_id = c.id
+        AND b.refseq = c.refseq
+        AND a.pos IS NOT NULL
+        AND a.genome_version = 'hg38'
     ORDER BY chrom, chromStart
-) TO '$FILENAME' WITH (FORMAT csv, DELIMITER E'\t', HEADER false)"
+) TO STDOUT WITH (FORMAT csv, DELIMITER E'\t', HEADER false)" > "${FILENAME}"
 
 if [ $? -eq 0 ]; then
     echo "Succès !"
